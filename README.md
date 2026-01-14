e5dd637768de0da34b9122aff6a0f274cc6a8e0a
jun <kanai.jun.1020@gmail.com>
2026-01-14 22:11:32 +0900

    chore: 開発環境整備 #1

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # mail_categorizer
+
+## What is 'mail_categorizer'
+
+
+## Quick Start
+### As a product
+if you just want to execute the application, just run the command:
+```sh
+./cmd.sh up
+```
+
+if you want to see the logs, run the command:
+```sh
+./cmd.sh logs
+```
+
+if you want to end the running application, run the command:
+```sh
+./cmd.sh down
+```
+
+### As a development
+This project is developed with devcontainer.
+So, if you want to develop this project, follow the instruction:
+
+1. Open this project in VSCode (*Extension 'Dev Containers' required)
+1. Click the blue button 'Reopen in Container' in the notification popup at the bottom right.
+    * The button does not appear? Don't worry, there is many ways to open in devcontainer. Please search and hope you solve that.
+1. Now your development environment has been configured!
+
+If you want to run the application in the container, run the command:
+```sh
+./cmd.sh run
+```
+
+## How to use
+
+## How it works
+
+## Note
+
+## References
---
+++ cmd.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+args=("$@")
+cmd="${args[0]}"
+cmd_args=("${args[@]:1}")
+
+dev_container_workdir="/app"
+
+check_is_inside_container () {
+    if [ "$(pwd)" != "$dev_container_workdir" ]; then
+        echo "This script should be run inside the container."
+        exit 1
+    fi
+}
+
+check_is_outside_container () {
+    if [ "$(pwd)" = "$dev_container_workdir" ]; then
+        echo "This script should be run on the host machine, not inside the container."
+        exit 1
+    fi
+}
+
+show_available_commands () {
+    echo "Available commands:"
+    if [ "$(pwd)" != "$dev_container_workdir" ]; then
+        echo "  up     - Start the application container"
+        echo "  down   - Stop the application container"
+        echo "  in     - Open a bash shell inside the application container"
+        echo "  logs   - View logs of the application container"
+    fi
+
+    if [ "$(pwd)" = "$dev_container_workdir" ]; then
+        echo "  run    - Run the application inside the development container"
+    fi
+}
+
+case "$cmd" in
+    "up")
+        check_is_outside_container
+        docker compose -f build/docker-compose.yaml up -d
+        ;;
+    "down")
+        check_is_outside_container
+        docker compose -f build/docker-compose.yaml down
+        ;;
+    "in")
+        check_is_outside_container
+        docker exec -it mail_categorizer_app /bin/bash
+        ;;
+    "logs")
+        check_is_outside_container
+        docker logs mail_categorizer_app
+        ;;
+    "run")
+        check_is_inside_container
+        ./entrypoint.sh
+        ;;
+    "help")
+        show_available_commands
+        ;;
+    *)
+        echo "Unknown command: $cmd"
+        show_available_commands
+        exit 1
+        ;;
+esac
